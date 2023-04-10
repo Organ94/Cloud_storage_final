049a555ce1e790706ccd858a520ae416d0f82102
Organ94 <99993473+Organ94@users.noreply.github.com>
2023-04-10 15:54:55 +0300

    Initial commit

---
+++ src/main/java/com/example/cloud_storage_finel/CloudStorageFinelApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloud_storage_finel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStorageFinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStorageFinelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cloud_storage_finel/CloudStorageFinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud_storage_finel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorageFinelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
